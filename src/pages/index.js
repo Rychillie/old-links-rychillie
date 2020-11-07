5e000b8235d433dbd5fbe5c8dd475cf8f12ba469
Rychillie <rychillie@hotmail.com>
2020-11-07 20:06:55 -0300

    image and SEO Title

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Hello World" />
+    <SEO title="Brazilian Frontend Developer" />
     <h1>Hello World</h1>
   </Layout>
 )

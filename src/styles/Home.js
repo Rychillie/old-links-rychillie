9b138976d0b436e85f52dc232821239dbaba0512
Rychillie <rychillie@hotmail.com>
2020-11-08 16:20:52 -0300

    content flex

--- src/styles/Home.js
+++ src/styles/Home.js
@@ -2,8 +2,12 @@ import styled from "styled-components"
 
 export const Content = styled.div`
   display: flex;
-  flex: 2;
+  flex: 1;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  @media screen and (min-width: 991px) {
+    flex: 2;
+  }
 `

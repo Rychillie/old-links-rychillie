d6d0b4de48744921e8ac3d7f223f2beda71a4dd6
Rychillie <rychillie@hotmail.com>
2020-11-14 20:43:34 -0300

    refactoring content

--- src/components/Apresentation/index.js
+++ src/components/Apresentation/index.js
@@ -1,14 +1,7 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import Img from "gatsby-image"
-
-import { Twitter } from "@styled-icons/boxicons-logos/Twitter"
-import { InstagramAlt } from "@styled-icons/boxicons-logos/InstagramAlt"
-import { Github } from "@styled-icons/boxicons-logos/Github"
-import { Twitch } from "@styled-icons/boxicons-logos/Twitch"
-import { Youtube } from "@styled-icons/boxicons-logos/Youtube"
-import { Telegram } from "@styled-icons/boxicons-logos/Telegram"
-import { Linkedin } from "@styled-icons/boxicons-logos/Linkedin"
+import Social from "../../content/social.js"
 
 import * as S from "./styled"
 
@@ -33,55 +26,16 @@ const Apresentation = () => {
         </S.Image>
         <h1>Hey, I'm Rychillie FrontEnd Developer and Content Creator.</h1>
         <S.Social>
-          <S.MyLink
-            href="https://twitter.com/rychillie"
-            title="Tiwitter"
-            target="_blank"
-          >
-            <Twitter />
-          </S.MyLink>
-          <S.MyLink
-            href="https://www.instagram.com/rychillie"
-            title="Instagram"
-            target="_blank"
-          >
-            <InstagramAlt />
-          </S.MyLink>
-          <S.MyLink
-            href="https://www.linkedin.com/in/rychillie"
-            title="Linkedin"
-            target="_blank"
-          >
-            <Linkedin />
-          </S.MyLink>
-          <S.MyLink
-            href="https://github.com/rychillie"
-            title="Github"
-            target="_blank"
-          >
-            <Github />
-          </S.MyLink>
-          <S.MyLink
-            href="https://www.twitch.tv/rychillie"
-            title="Twitch"
-            target="_blank"
-          >
-            <Twitch />
-          </S.MyLink>
-          <S.MyLink
-            href="https://www.youtube.com/rychillie"
-            title="YouTube"
-            target="_blank"
-          >
-            <Youtube />
-          </S.MyLink>
-          <S.MyLink
-            href="https://t.me/s/rychiii"
-            title="Telegram"
-            target="_blank"
-          >
-            <Telegram />
-          </S.MyLink>
+          {Social.map(item => (
+            <S.MyLink
+              key={item.id}
+              href={`${item.link}`}
+              title={`${item.title}`}
+              target="_blank"
+            >
+              {item.icon}
+            </S.MyLink>
+          ))}
         </S.Social>
       </S.Content>
     </S.Header>
---
+++ src/content/social.js
@@ -0,0 +1,49 @@
+import React from "react"
+
+import { Twitter } from "@styled-icons/boxicons-logos/Twitter"
+import { InstagramAlt } from "@styled-icons/boxicons-logos/InstagramAlt"
+import { Github } from "@styled-icons/boxicons-logos/Github"
+import { Twitch } from "@styled-icons/boxicons-logos/Twitch"
+import { Youtube } from "@styled-icons/boxicons-logos/Youtube"
+import { Telegram } from "@styled-icons/boxicons-logos/Telegram"
+import { Linkedin } from "@styled-icons/boxicons-logos/Linkedin"
+
+const Social = [
+  {
+    title: "Twitter",
+    icon: <Twitter />,
+    link: "https://www.twitter.com/rychillie",
+  },
+  {
+    title: "Instagram",
+    icon: <InstagramAlt />,
+    link: "https://www.instagram.com/rychillie",
+  },
+  {
+    title: "Linkedin",
+    icon: <Linkedin />,
+    link: "https://www.linkedin.com/in/rychillie",
+  },
+  {
+    title: "Github",
+    icon: <Github />,
+    link: "https://www.github.com/rychillie",
+  },
+  {
+    title: "Twitch",
+    icon: <Twitch />,
+    link: "https://www.twitch.tv/rychillie",
+  },
+  {
+    title: "YouTube",
+    icon: <Youtube />,
+    link: "https://www.youtube.com/rychillie",
+  },
+  {
+    title: "Telegram",
+    icon: <Telegram />,
+    link: "https://t.me/s/rychiii",
+  },
+]
+
+export default Social

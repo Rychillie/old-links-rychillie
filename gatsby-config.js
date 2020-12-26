0de80b0fc48fcca94727a565887e7f45d26ed0d9
Rychillie <rychillie@hotmail.com>
2020-12-26 17:11:05 -0300

    google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,14 @@ module.exports = {
       },
     },
     `gatsby-plugin-styled-components`,
+
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-45575876-4",
+        head: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,

b5b04b5c5acc5a64fc9afc14f5064473e02b500d
Rychillie <rychillie@hotmail.com>
2021-01-21 15:14:34 -0300

    volta

--- src/html.js
+++ src/html.js
@@ -12,7 +12,6 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         {props.headComponents}
-        <script async src="https://app.havepeople.com/connector.js"></script>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
